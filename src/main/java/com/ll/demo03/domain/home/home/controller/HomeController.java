6baae68d0db793d3b7d0b61ac734349ecf6e6b49
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-13 22:57:06 +0900

    test6

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "자동배포 테스트5....., on " + siteName;
+        return "자동배포 테스트6....., on " + siteName;
     }
 
     @GetMapping("/secretKey")
