8df60b0325c302b8752e61e678352dd68138a02f
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-14 12:04:37 +0900

    new test

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "wowowowwowow, on " + siteName;
+        return "new test..., on " + siteName;
     }
 
     @GetMapping("/secretKey")
